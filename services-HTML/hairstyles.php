06408b25e29355cba2201069bc72df70453e570f
Ero <khachatryanero.2002@mail.ru>
2022-05-30 21:51:00 +0400

    add Jadore project

---
+++ services-HTML/hairstyles.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="hy">
+
+<head>
+	<?php include('../nullFiles/styles&meta2.php'); ?>
+	<!-- Имя сайта-->
+	<title>Սանրվածքներ -- Jadore BS</title>
+</head>
+
+<body id="body">
+
+	<!-- Загрузчик -->
+	<div id="preloader"></div>
+
+	<!-- Оболоча -->
+	<div class="wrapper" id="wrapper">
+
+		<!-- Шапка сайта старт -->
+		<header class="header" id="header">
+			<div class="header__container">
+				<div class="header__body">
+					<a href="../index.php" class="header__logo" data-aos="fade-in">
+						<img src="../img/logo.png" alt="Լոգո">
+					</a>
+					<div class="header__burger" id="toggle">
+						<span></span>
+					</div>
+					<nav class="header__menu menu" id="sidebar" data-aos="fade-left">
+						<ul class="menu__list">
+							<li class="menu__item"><a href="../index.php" class="menu__link">Գլխավոր</a></li>
+							<li class="menu__item"><a href="../services.php" class="menu__link active">Ծառայություններ</a></li>
+							<li class="menu__item"><a href="../works.php" class="menu__link">Աշխատանք</a></li>
+							<li class="menu__item"><a href="../contacts.php" class="menu__link">Կապ</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<!-- Шапка сайта конец -->
+
+		<!-- Контент -->
+		<main>
+			<!-- Раздел ГЛАВНАЯ старт -->
+			<section class="home" data-aos="fade-in">
+				<div class="home__bg home__bg-3">
+					<div class="home__container">
+						<div class="home__body body-home body-home-2">
+							<div class="body-home__title">
+								Սանրվածքներ
+							</div>
+							<div class="body-home__text">
+								Այժմ դուք չպետք է անհանգստանաք արդյունքի համար: Երկար տարիների փորձով մեր սրահի մասնագետները
+								կստեղծեն բացառիկ կերպար, որը կընդգծի Ձեր գեղեցկությունը։
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел ГЛАВНАЯ конец -->
+
+			<!-- Раздел Услуги всех старт -->
+			<section class="services-heading">
+				<div class="services-heading__container">
+					<div class="services-heading__block block-heading__services">
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/hair-dryer-svgrepo-com.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								ԵՐԿԱՐԱԺԱՄԿԵՏ ՓՈՐՁ ԵՎ ՄԱՍՆԱԳԻՏՈՒԹՅՈՒՆ
+							</div>
+						</div>
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/hair-comb-svgrepo-com.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								Անվտանգ ծառայություն
+							</div>
+						</div>
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/cosmetics-svgrepo-com.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								Աշխարհի լավագույն բրենդների կոսմետիկան
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел Услуги всех конец -->
+
+			<!-- Раздел Цены на Стрижки старт -->
+			<section class="services-pricing">
+				<div class="services-pricing__container">
+					<div class="services-pricing__heading heading">
+						Արժեքները
+					</div>
+					<div class="services-pricing__block">
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Տղամարդու կտրվածք
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Տղամարդկանց կտրվածք + լվացում շամպունով
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+							<div class="item-pricing__price">
+								2500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Կանացի կտրվածք
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Կանացի կտրվածք + լվացում շամպունով
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+							<div class="item-pricing__price">
+								2500դր.
+							</div>
+							<div class="item-pricing__price">
+								3000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Պատվերով կտրվածք
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Մազերի հարդարում
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Երեկոյան սանրվածք/պոչ/հոլիվուդյան ալիք
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Հյուսել
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+							<div class="item-pricing__price">
+								2500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Գանգրահեցնել
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+						</div>
+					</div>
+					<div class="services-pricing__posible">
+						<strong>*Արժեքը կախված է թե որ տեսակը դուք կնտրեք և ծառայությունների վրա ծախսված նյութերի
+							քանակից</strong>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел Цены на Стрижки конец -->
+		</main>
+
+
+		<!-- Подвал сайта старт -->
+		<?php include('../nullFiles/footer2.php'); ?>
+
+
+	</div>
+
+
+	<!-- Скрипти (JS) -->
+	<script src="https://kit.fontawesome.com/364482cb4d.js" type="text/javascript"></script>
+	<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="../assets/aos/js/aos.js"></script>
+	<script src="../assets/Owl-Carousel/js/owl.carousel.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+	<script src="../js/main.js"></script>
+</body>
+
+</html>
---
+++ services-HTML/manicure.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="hy">
+
+<head>
+	<?php include('../nullFiles/styles&meta2.php'); ?>
+	<!-- Имя сайта-->
+	<title>Մանիկյուռ և պեդիկյուռ-- Jadore BS</title>
+</head>
+
+<body id="body">
+
+	<!-- Загрузчик -->
+	<div id="preloader"></div>
+
+	<!-- Оболоча -->
+	<div class="wrapper" id="wrapper">
+
+		<!-- Шапка сайта старт -->
+		<header class="header" id="header">
+			<div class="header__container">
+				<div class="header__body">
+					<a href="../index.php" class="header__logo" data-aos="fade-in">
+						<img src="../img/logo.png" alt="Լոգո">
+					</a>
+					<div class="header__burger" id="toggle">
+						<span></span>
+					</div>
+					<nav class="header__menu menu" id="sidebar" data-aos="fade-left">
+						<ul class="menu__list">
+							<li class="menu__item"><a href="../index.php" class="menu__link">Գլխավոր</a></li>
+							<li class="menu__item"><a href="../services.php" class="menu__link active">Ծառայություններ</a></li>
+							<li class="menu__item"><a href="../works.php" class="menu__link">Աշխատանք</a></li>
+							<li class="menu__item"><a href="../contacts.php" class="menu__link">Կապ</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		<!-- Шапка сайта конец -->
+
+		<!-- Контент -->
+		<main>
+			<!-- Раздел ГЛАВНАЯ старт -->
+			<section class="home" data-aos="fade-in">
+				<div class="home__bg home__bg-3">
+					<div class="home__container">
+						<div class="home__body body-home body-home-2">
+							<div class="body-home__title">
+								Մանիկյուռ և պեդիկյուռ
+							</div>
+							<div class="body-home__text">
+								Մենք մեր հաճախորդներին առաջարկում ենք անվտանգ մատնահարդարում և պեդիկյուր, տարբեր տեսակի
+								ծածկույթներ, ինչպես նաև եղունգների երկարացում:
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел ГЛАВНАЯ конец -->
+
+			<!-- Раздел Услуги всех старт -->
+			<section class="services-heading">
+				<div class="services-heading__container">
+					<div class="services-heading__block block-heading__services">
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/stop.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								Մատնահարդարում, Պեդիկյուր, ՁԵՌՔԻ ԵՎ ՈՏՔԻ ԲՈՒԺՈՒՄՆԵՐ:
+							</div>
+						</div>
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/manicure.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								ԼԱՎԱԳՈՒՅՆ ԲՐԵՆԴՆԵՐԻ ՄԱՆԻԿՅՈՒՌՆԵՐ:
+							</div>
+						</div>
+						<div class="block-heading__services-item item-heading__services">
+							<div class="item-services__icon">
+								<img src="../img/services/cut.svg" width="50" height="50" alt="">
+							</div>
+							<div class="item-services__heading">
+								ԳՈՐԾԻՔՆԵՐԸ ԱԽՏԱՀԱՆՎՈՒՄ ԵՆ ԱՄԵՆ ՍՊԱՍԱՐԿՈՒՄԻՑ ԱՌԱՋ:
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел Услуги всех конец -->
+
+			<!-- Раздел Цены на Стрижки старт -->
+			<section class="services-pricing">
+				<div class="services-pricing__container">
+					<div class="services-pricing__heading heading">
+						Արժեքները
+					</div>
+					<div class="services-pricing__block flex">
+						<div class="services-pricing__block-subtitle">
+							<h3>ՄԱՆԻԿՅՈՒՌ / ՊԵԴԻԿՅՈՒՌ</h3>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Մատնահարդարում դասական
+							</div>
+							<div class="item-pricing__price">
+								1200դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Մատնահարդարում Շիլակ
+							</div>
+							<div class="item-pricing__price">
+								2500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Պեդիկյուր դասական
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Պեդիկյուր սարքավորումով
+							</div>
+							<div class="item-pricing__price">
+								2000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Պեդիկյուր Շիլակ
+							</div>
+							<div class="item-pricing__price">
+								3000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Շիլակի հեռացում
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Չիստկա
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Գել-լաքի ծածկույթ
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								Եղունգների ամրացում
+							</div>
+							<div class="item-pricing__price">
+								1000դր.
+							</div>
+						</div>
+						<div class="services-pricing__item item-pricing">
+							<div class="item-pricing__title">
+								SPA ձեռքերի խնամք
+							</div>
+							<div class="item-pricing__price">
+								1500դր.
+							</div>
+						</div>
+					</div>
+					<div class="services-pricing__posible">
+						<strong>*Արժեքը կախված է թե որ տեսակը դուք կնտրեք և ծառայությունների վրա ծախսված նյութերի
+							քանակից</strong>
+					</div>
+				</div>
+			</section>
+			<!-- Раздел Цены на Стрижки конец -->
+		</main>
+
+
+		<!-- Подвал сайта старт -->
+		<?php include('../nullFiles/footer2.php'); ?>
+
+
+	</div>
+
+
+	<!-- Скрипти (JS) -->
+	<script src="https://kit.fontawesome.com/364482cb4d.js" type="text/javascript"></script>
+	<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="../assets/aos/js/aos.js"></script>
+	<script src="../assets/Owl-Carousel/js/owl.carousel.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+	<script src="../js/main.js"></script>
+</body>
+
+</html>
